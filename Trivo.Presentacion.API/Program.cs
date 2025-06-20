f7b77f9da8ad771f8aa6d9100a33699a1e90178c
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-06-19 21:07:03 -0400

    feat: add Serilog

--- Trivo.Presentacion.API/Program.cs
+++ Trivo.Presentacion.API/Program.cs
@@ -1,29 +1,48 @@
+using Serilog;
 using Trivo.Infraestructura.Persistencia;
 
-var builder = WebApplication.CreateBuilder(args);
+try 
+{ 
+    Log.Information("Iniciando servidor");
 
-// Add services to the container.
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
 
-builder.Services.AgregarPesistencia(builder.Configuration);
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AgregarPesistencia(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex,"Ha ocurrido un error");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
