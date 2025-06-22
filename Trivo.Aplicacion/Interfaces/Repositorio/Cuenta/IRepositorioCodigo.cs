d850562e07fbe66ccf502ae0e0cb704eae7a66ad
Dayron <Dayronbp06@gmail.com>
2025-06-22 17:33:55 -0400

    chore: rename methods to spanish

--- Trivo.Aplicacion/Interfaces/Repositorio/Cuenta/IRepositorioCodigo.cs
+++ Trivo.Aplicacion/Interfaces/Repositorio/Cuenta/IRepositorioCodigo.cs
@@ -4,12 +4,12 @@ namespace Trivo.Aplicacion.Interfaces.Repositorio.Cuenta;
 
 public interface IRepositorioCodigo
 {
-    Task CreateCodigoAsync(Codigo codigo, CancellationToken cancellationToken);
+    Task CrearCodigoAsync(Codigo codigo, CancellationToken cancellationToken);
     Task<Codigo> ObtenerCodigoPorIdAsync(Guid id, CancellationToken cancellationToken);
     Task <Codigo> BuscarCodigoAsync(string codigo, CancellationToken cancellationToken);
-    Task EliminarTokenAsync(Codigo codigo, CancellationToken cancellationToken);
-    Task<bool> ExistsCodeAsync(string codigo, CancellationToken cancellationToken);
-    Task<bool> IsCodeValidAsync(string codigo,CancellationToken cancellationToken);
-    Task MarkCodeAsUsedAsync(string codigo, CancellationToken cancellationToken);
-    Task<bool> IsCodeUnUsedAsync(string codigo, CancellationToken cancellationToken);
+    Task EliminarCodigoAsync(Codigo codigo, CancellationToken cancellationToken);
+    Task<bool> ExisteElCodigoAsync(string codigo, CancellationToken cancellationToken);
+    Task<bool> CodigoEsValidoAsync(string codigo,CancellationToken cancellationToken);
+    Task MarcarCodigoComoUsado(string codigo, CancellationToken cancellationToken);
+    Task<bool> CodigoNoUsadoAsync(string codigo, CancellationToken cancellationToken);
 }
--- Trivo.Infraestructura.Persistencia/Repositorio/Cuenta/RepositorioCodigo.cs
+++ Trivo.Infraestructura.Persistencia/Repositorio/Cuenta/RepositorioCodigo.cs
@@ -7,7 +7,7 @@ namespace Trivo.Infraestructura.Persistencia.Repositorio.Cuenta;
 
 public class RepositorioCodigo(TrivoContexto contexto): RepositorioGenerico<Codigo>(contexto), IRepositorioCodigo
 {
-    public async Task CreateCodigoAsync(Codigo codigo, CancellationToken cancellationToken)
+    public async Task CrearCodigoAsync(Codigo codigo, CancellationToken cancellationToken)
     {
         await _trivoContexto.Set<Codigo>().AddAsync(codigo, cancellationToken);
         await _trivoContexto.SaveChangesAsync(cancellationToken);
@@ -24,13 +24,13 @@ public class RepositorioCodigo(TrivoContexto contexto): RepositorioGenerico<Codi
             .AsNoTracking()
             .FirstOrDefaultAsync(c => c.Valor == codigo, cancellationToken);
 
-    public async Task EliminarTokenAsync(Codigo codigo, CancellationToken cancellationToken)
+    public async Task EliminarCodigoAsync(Codigo codigo, CancellationToken cancellationToken)
     {
         _trivoContexto.Set<Codigo>().Remove(codigo);
         await _trivoContexto.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task<bool> ExistsCodeAsync(string codigo, CancellationToken cancellationToken) =>
+    public async Task<bool> ExisteElCodigoAsync(string codigo, CancellationToken cancellationToken) =>
         await _trivoContexto.Set<Codigo>()
             .AsNoTracking()
             .AnyAsync(c => c.Valor == codigo, cancellationToken);
@@ -38,7 +38,7 @@ public class RepositorioCodigo(TrivoContexto contexto): RepositorioGenerico<Codi
     public async Task<bool> IsCodeValidAsync(string codigo, CancellationToken cancellationToken) =>
         await ValidarAsync(c => c.Valor == codigo, cancellationToken);
 
-    public async Task MarkCodeAsUsedAsync(string codigo, CancellationToken cancellationToken)
+    public async Task MarcarCodigoComoUsado(string codigo, CancellationToken cancellationToken)
     {
        var usuarioCodigo = await _trivoContexto.Set<Codigo>()
            .FirstOrDefaultAsync(c => c.Valor == codigo, cancellationToken);
@@ -50,7 +50,7 @@ public class RepositorioCodigo(TrivoContexto contexto): RepositorioGenerico<Codi
        }
     }
 
-    public async Task<bool> IsCodeUnUsedAsync(string codigo, CancellationToken cancellationToken) =>
+    public async Task<bool> CodigoNoUsadoAsync(string codigo, CancellationToken cancellationToken) =>
         await _trivoContexto.Set<Codigo>()
             .AsNoTracking()
             .AnyAsync(c => c.Valor == codigo && c.Usado == false, cancellationToken);
