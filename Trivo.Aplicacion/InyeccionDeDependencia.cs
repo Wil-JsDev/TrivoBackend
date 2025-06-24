0fa6d120bc2c6db56b9c4d40c18316a75c6aacd2
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-06-23 22:38:14 -0400

    feat: configure application layer with MediatR, FluentValidation, and ProblemDetails

---
+++ Trivo.Aplicacion/InyeccionDeDependencia.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Trivo.Aplicacion.Comportamiento;
+
+namespace Trivo.Aplicacion;
+
+public static class InyeccionDeDependencia
+{
+    public static void AgregarCapaAplicacion(this IServiceCollection servicio)
+    {
+        servicio.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        servicio.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidacionComportamiento<,>));
+        });
+        servicio.AddProblemDetails();
+    }
+}
