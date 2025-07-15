7be293a08a4e97c51a294786b71ba137b66e47c4
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-15 19:04:47 -0400

    feat: add query handlers for user-related operations

---
+++ Trivo.Aplicacion/Modulos/Usuario/Querys/ObtenerBiografia/ObtenerBiografiaPorUsuarioIdQueryHandler.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Trivo.Aplicacion.Abstracciones.Mensajes;
+using Trivo.Aplicacion.DTOs.Cuentas.Usuarios;
+using Trivo.Aplicacion.Interfaces.Repositorio.Cuenta;
+using Trivo.Aplicacion.Utilidades;
+
+namespace Trivo.Aplicacion.Modulos.Usuario.Querys.ObtenerBiografia;
+
+internal sealed class ObtenerBiografiaPorUsuarioIdQueryHandler(
+    ILogger<ObtenerBiografiaPorUsuarioIdQueryHandler> logger,
+    IRepositorioUsuario repositorioUsuario,
+    IDistributedCache cache   
+    ) : IQueryHandler<ObtenerBiografiaPorUsuarioIdQuery, BiografiaUsuarioDto>
+{
+    public async Task<ResultadoT<BiografiaUsuarioDto>> Handle(ObtenerBiografiaPorUsuarioIdQuery request, CancellationToken cancellationToken)
+    {
+        var usuario = await cache.ObtenerOCrearAsync($"obtener-biografia-por-usuario-id-{request.UsuarioId}", 
+            async () => await repositorioUsuario.ObtenerByIdAsync(request.UsuarioId, cancellationToken), 
+            cancellationToken: cancellationToken);
+        
+        if (usuario is null)
+        {
+            logger.LogWarning("No se encontró el usuario para obtener su foto de perfil.");
+    
+            return ResultadoT<BiografiaUsuarioDto>.Fallo(Error.NoEncontrado("404", "El usuario no existe"));
+        }
+        
+        return ResultadoT<BiografiaUsuarioDto>.Exito(new BiografiaUsuarioDto(usuario.Biografia));
+    }
+}
