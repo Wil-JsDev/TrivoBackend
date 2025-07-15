0e6ad60535920168fec7bbd37bd4d9d9c8ddceda
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-14 23:34:52 -0400

    faet: add user mapper

---
+++ Trivo.Aplicacion/Mapper/UsuarioMapper.cs
@@ -0,0 +1,19 @@
+using Trivo.Aplicacion.DTOs.Cuentas.Usuarios;
+using Trivo.Aplicacion.DTOs.Habilidades;
+using Trivo.Aplicacion.DTOs.Intereses;
+
+namespace Trivo.Aplicacion.Mapper;
+
+public static class UsuarioMapper
+{
+    public static UsuarioReconmendacionDto MapToDto(Dominio.Modelos.Usuario entidad)
+    {
+        return new UsuarioReconmendacionDto(
+            UsuarioId: entidad.Id ?? Guid.Empty,
+            Nombre: entidad.Nombre,
+            Intereses: entidad.UsuarioInteres?.Select(i => new InteresConIdDto(i.Interes?.Id ?? Guid.Empty, i.Interes?.Nombre ?? "")).ToList() ?? new List<InteresConIdDto>(),
+            Habilidades: entidad.UsuarioHabilidades?.Select(h => new HabilidadConIdDto(h.Habilidad?.HabilidadId ?? Guid.Empty, h.Habilidad?.Nombre ?? "")).ToList() ?? new List<HabilidadConIdDto>(),
+            FotoPerfil: entidad.FotoPerfil
+        );
+    }
+}
