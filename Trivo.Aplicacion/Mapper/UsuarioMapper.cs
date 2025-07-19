ebecab7588cbe8523c49b6d0c85c73bdb24e1b07
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-19 00:12:58 -0400

    feat: add last name and bio

--- Trivo.Aplicacion/Mapper/UsuarioMapper.cs
+++ Trivo.Aplicacion/Mapper/UsuarioMapper.cs
@@ -11,6 +11,8 @@ public static class UsuarioMapper
         return new UsuarioReconmendacionDto(
             UsuarioId: entidad.Id ?? Guid.Empty,
             Nombre: entidad.Nombre,
+            Apellido: entidad.Apellido,
+            Biografia: entidad.Biografia,
             Intereses: entidad.UsuarioInteres?.Select(i => new InteresConIdDto(i.Interes?.Id ?? Guid.Empty, i.Interes?.Nombre ?? "")).ToList() ?? new List<InteresConIdDto>(),
             Habilidades: entidad.UsuarioHabilidades?.Select(h => new HabilidadConIdDto(h.Habilidad?.HabilidadId ?? Guid.Empty, h.Habilidad?.Nombre ?? "")).ToList() ?? new List<HabilidadConIdDto>(),
             FotoPerfil: entidad.FotoPerfil
