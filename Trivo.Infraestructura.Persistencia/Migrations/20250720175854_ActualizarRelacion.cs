176262e4500b813a54f18dba669b876a678d6cfc
Dayron <Dayronbp06@gmail.com>
2025-07-20 14:00:04 -0400

    fix: update relationship

---
+++ Trivo.Infraestructura.Persistencia/Migrations/20250720175854_ActualizarRelacion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trivo.Infraestructura.Persistencia.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizarRelacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
