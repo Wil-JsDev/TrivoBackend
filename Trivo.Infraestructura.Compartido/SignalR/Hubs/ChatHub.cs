b30743b1e307a5902bfc17eb459ee6c1f943cd39
Dayron <Dayronbp06@gmail.com>
2025-07-19 22:24:51 -0400

    fix: improve chat and message retrieval logic

--- Trivo.Infraestructura.Compartido/SignalR/Hubs/ChatHub.cs
+++ Trivo.Infraestructura.Compartido/SignalR/Hubs/ChatHub.cs
@@ -1,25 +1,38 @@
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Trivo.Aplicacion.DTOs.Mensaje;
+using Trivo.Aplicacion.Modulos.Chat.Querys.Paginacion;
+using Trivo.Aplicacion.Modulos.Mensajes.Querys;
 
 namespace Trivo.Infraestructura.Compartido.SignalR.Hubs;
 
 [Authorize]
 public class ChatHub(
-    ILogger<ChatHub> logger
+    ILogger<ChatHub> logger,
+    IMediator mediator
     ): Hub<IChatHub>
 {
-    public override Task OnConnectedAsync()
+    public override async Task OnConnectedAsync()
     {
-        logger.LogInformation($"Usuario conectado: {Context.UserIdentifier}");
-        return base.OnConnectedAsync();
+        if (!Guid.TryParse(Context.UserIdentifier, out var userId))
+        {
+            logger.LogWarning("UserIdentifier no es un GUID válido: {UserIdentifier}", Context.UserIdentifier);
+            return;
+        }
+        
+        logger.LogInformation($"Usuario conectado: {userId}");
+        
+        await ObtenerChatsUsuario();
+        await base.OnConnectedAsync();
     }
     
-    public override Task OnDisconnectedAsync(Exception? exception)
+    public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        logger.LogInformation($"Usuario desconectado: {Context.UserIdentifier}");
-        return base.OnDisconnectedAsync(exception);
+        var userId = Context.UserIdentifier;
+        logger.LogInformation($"Usuario desconectado: {userId}");
+        await base.OnDisconnectedAsync(exception);
     }
     
     public async Task EnviarMensaje(MensajeDto mensaje)
@@ -37,5 +50,29 @@ public class ChatHub(
 
         await Clients.User(mensaje.ReceptorId.ToString())
             .RecibirMensajePrivado(mensaje with { EmisorId = emisorGuid });
+    }
+    public async Task ObtenerChatsUsuario(int numeroPagina = 1, int tamanoPagina = 10)
+    {
+        if (!Guid.TryParse(Context.UserIdentifier, out var usuarioId))
+            return;
+
+        var chats = await mediator.Send(new ObtenerPaginasChatQuery(
+            usuarioId,
+            numeroPagina,
+            tamanoPagina
+        ));
+
+        await Clients.Caller.RecibirChats(chats.Valor);
+    }
+
+    public async Task ObtenerMensajesChat(Guid chatId, int numeroPagina = 1, int tamanoPagina = 20)
+    {
+        var mensajes = await mediator.Send(new ObtenerPaginasMensajesQuery(
+            chatId,
+            numeroPagina,
+            tamanoPagina
+        ));
+
+        await Clients.Caller.RecibirMensajesDelChat(chatId, mensajes.Valor);
     }
 }
