78bbf6c4cad025b42d9563d17a003e2b51739864
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-01 16:13:29 -0400

    feat: add notification service DI

--- Trivo.Aplicacion/InyeccionDeDependencia.cs
+++ Trivo.Aplicacion/InyeccionDeDependencia.cs
@@ -24,6 +24,7 @@ public static class InyeccionDeDependencia
 
             servicio.AddScoped<ICodigoServicio, CodigoServicio>();
             servicio.AddScoped<IValidarCorreo, ValidarCorreo>();
+            servicio.AddScoped<INotificacionServicio, NotificacionServicio>();
             
         #endregion
         
