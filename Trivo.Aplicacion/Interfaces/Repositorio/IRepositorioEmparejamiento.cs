619901dd38ac6215ca17b8d6ea5b2a0b6286521b
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-03 16:40:09 -0400

    feat(queries): eager load expert/recruiter relations for matches

--- Trivo.Aplicacion/Interfaces/Repositorio/IRepositorioEmparejamiento.cs
+++ Trivo.Aplicacion/Interfaces/Repositorio/IRepositorioEmparejamiento.cs
@@ -43,4 +43,6 @@ public interface IRepositorioEmparejamiento : IRepositorioGenerico<Emparejamient
     /// <param name="cancellationToken">Token para cancelar la operación asincrónica si es necesario.</param>
     /// <returns>Una lista de emparejamientos pendientes asociados al usuario como reclutador.</returns>
     Task<IEnumerable<Emparejamiento>> ObtenerEmparejamientosComoReclutadorAsync(Guid usuarioId, CancellationToken cancellationToken);
+    
+    Task<Emparejamiento?> ObtenerPorIdAsync(Guid emparejamientoId, CancellationToken cancellationToken);
 }
