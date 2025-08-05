4d205c25f1f151f2a7f6c190f87694f2aeeabee9
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-04 23:27:10 -0400

    feat: add report dto

---
+++ Trivo.Aplicacion/DTOs/Reportes/ReporteDto.cs
@@ -0,0 +1,28 @@
+namespace Trivo.Aplicacion.DTOs.Reportes;
+
+public sealed record MensajeDtoParaReporte
+(
+    Guid? MensajeId,
+    Guid? EmisorId,
+    string? Contenido,
+    string? Tipo,
+    DateTime? FechaEnvio,
+    UsuarioDtoParaReporte? Emisor
+);
+
+public sealed record UsuarioDtoParaReporte
+(
+    Guid UsuarioId,
+    string Nombre
+);
+
+public sealed record ReporteDto
+(
+    Guid ReporteId,
+    Guid? ReportadoPor,
+    Guid? MensajeId,
+    string? Nota,
+    string? EstadoReporte,
+    MensajeDtoParaReporte? Mensaje, // Informacion del mensaje reportado con la informacion del usuario que lo reporto
+    UsuarioDtoParaReporte? UsuarioReportadoPor  // Información del usuario que hizo el reporte
+);
