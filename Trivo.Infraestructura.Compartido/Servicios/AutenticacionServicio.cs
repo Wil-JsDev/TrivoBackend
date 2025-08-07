713690e39753b8748d1dd1f7a9670ec6957aa1f3
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-07 16:23:10 -0400

    feat: add new claims

--- Trivo.Infraestructura.Compartido/Servicios/AutenticacionServicio.cs
+++ Trivo.Infraestructura.Compartido/Servicios/AutenticacionServicio.cs
@@ -70,12 +70,15 @@ public class AutenticacionServicio(
     
     public string GenerarTokenAdministrador(Administrador admin)
     {
+        var nombreCompleto = $"{admin.Nombre} {admin.Apellido}";
         var claims = new List<Claim>
         {
             new Claim(JwtRegisteredClaimNames.Sub, admin.Id.ToString()!),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(JwtRegisteredClaimNames.Email, admin.Email!),
             new Claim("nombreUsuario", admin.NombreUsuario!),
+            new Claim("nombreCompleto", nombreCompleto),
+            new Claim("fotoPerfil", admin.FotoPerfil!),
             new Claim("roles", Roles.Administrador.ToString()),
             new Claim("tipo", "access")
         };
